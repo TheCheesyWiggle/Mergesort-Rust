239eb1c92ed4e3ec5dd2a579ddc3f91403ce1d1a
TheCheesyWiggle <74103207+TheCheesyWiggle@users.noreply.github.com>
2022-02-03 21:16:34 +0000

    fixed lifetime identifier issue -need to fix scope issues with mid -need to fix mismatched datatype issues now

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,16 @@ fn main() {
 //start of the mergesort
 //-> &mut [i32] sets function type to &mut [i32]
 fn merge_sort(array: &mut [i32]) -> &mut [i32] {
-    //sets the middle
-    let mid = array.len()/2;
     //sets up left and right array
     let &mut left = [mid..];
     let &mut right = [..mid];
+    //sets the middle
+    let mid = array.len()/2;
     //checks if the array is larger than 2
-    if array.len()>=2{
-        //sets up left and right array
-        let &mut left = [mid..];
-        let &mut right = [..mid];
-    }
-    else {
+    if array.len()<=2{
         //returns array if there is one/no values in it
         return array;
     }
-
     //recurses through
     merge_sort(left);
     merge_sort(right);
